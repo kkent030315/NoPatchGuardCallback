4e6e9f798d46795bc1c92e9b47f061f2ca2f0333
kento <37926134+kkent030315@users.noreply.github.com>
2021-05-22 11:31:57 +0900

    initial commit

---
+++ noPGcallback/main.c
@@ -0,0 +1,268 @@
+/*
+
+	MIT License
+
+	Copyright (c) 2021 Kento Oki
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+*/
+
+#include "main.h"
+
+PVOID NpgQueryModuleImageBase( PCHAR ModuleName )
+{
+    NTSTATUS ntStatus = STATUS_SUCCESS;
+	PVOID Address = 0;
+	ULONG NeededSize = 0;
+	
+    ntStatus = ZwQuerySystemInformation( 
+		SystemModuleInformation, 0, 0, &NeededSize );
+	
+    if ( ntStatus != STATUS_INFO_LENGTH_MISMATCH )
+    {
+		NPGCB_KDPRINT( "ZwQuerySystemInformation Failed\n" );
+		return Address;
+	}
+
+    PRTL_PROCESS_MODULES modules = ( PRTL_PROCESS_MODULES )
+		ExAllocatePool( NonPagedPool, NeededSize );
+
+	if ( !modules )
+    {
+		NPGCB_KDPRINT( "Failed to allocate pool\n" );
+		return Address;
+	}
+
+    ntStatus = ZwQuerySystemInformation(
+        SystemModuleInformation, modules, NeededSize, 0 );
+
+	if ( !NT_SUCCESS( ntStatus ) )
+    {
+		NPGCB_KDPRINT( "ZwQuerySystemInformation Failed\n" );
+		ExFreePool( modules );
+		return Address;
+	}
+
+	for ( ULONG i = 0; i < modules->NumberOfModules; ++i )
+	{
+        RTL_PROCESS_MODULE_INFORMATION entry = modules->Modules[ i ];
+
+		if ( strstr( ( PCHAR )entry.FullPathName, ModuleName ) )
+		{
+			Address = entry.ImageBase;
+			break;
+		}
+	}
+
+	ExFreePool( modules );
+	return Address;
+}
+
+//
+// the callback receiver
+//
+VOID PsCreateProcessNotifyCallback(
+	IN HANDLE ParentId,
+	IN HANDLE ProcessId,
+	IN BOOLEAN Create )
+{
+	NPGCB_KDPRINT( "PsCreateProcessNotifyCallback Called\n" );
+}
+
+NTSTATUS NpgInitialize()
+{
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+
+	//
+	// the ntoskrnl image base
+	//
+	PVOID NtosKrnlImageBase =
+		NpgQueryModuleImageBase( "ntoskrnl.exe" );
+
+	if ( !NtosKrnlImageBase )
+	{
+		NPGCB_KDPRINT( "Failed to locate ntoskrnl image base\n" );
+		return STATUS_UNSUCCESSFUL;
+	}
+
+	//
+	// both ExAllocateCallBack and ExCompareExchangeCallBack are
+	// undocumented NT Kernel internal function
+	//
+	// these RVAs are might not be work on another Windows builds
+	// or cause system crash
+	//
+
+	*( PVOID* )( &ExAllocateCallBack ) =
+		( PVOID )( ( UINT_PTR )NtosKrnlImageBase + RVA_EX_ALLOCATE_CALLBACK );
+
+	*( PVOID* )( &ExCompareExchangeCallBack ) =
+		( PVOID )( ( UINT_PTR )NtosKrnlImageBase + RVA_EX_COMPARE_EXCHANGE_CALLBACK );
+
+	NPGCB_KDPRINT( "ExAllocateCallBack @ 0x%p\n", ExAllocateCallBack );
+	NPGCB_KDPRINT( "ExCompareExchangeCallBack @ 0x%p\n", ExCompareExchangeCallBack );
+
+	if ( !ExAllocateCallBack || !ExCompareExchangeCallBack )
+	{
+		NPGCB_KDPRINT( "Failed to locate NT kernel internal functions\n" );
+		return STATUS_UNSUCCESSFUL;
+	}
+
+	return ntStatus;
+}
+
+NTSTATUS NpgRegisterCallback( 
+	PCREATE_PROCESS_NOTIFY_ROUTINE NotifyRoutine )
+{
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+
+	NPGCB_KDPRINT( "NpgRegisterCallback Called\n" );
+	NPGCB_KDPRINT( " ---> NotifyRoutine: 0x%p\n", NotifyRoutine );
+
+	PVOID NtosKrnlImageBase =
+		NpgQueryModuleImageBase( "ntoskrnl.exe" );
+
+	if ( !NtosKrnlImageBase )
+	{
+		NPGCB_KDPRINT( "Failed to locate ntoskrnl image base\n" );
+		return STATUS_UNSUCCESSFUL;
+	}
+
+	NPGCB_KDPRINT( "ntoskrnl @ %p\n", NtosKrnlImageBase );
+
+	//
+	// allocate the new callback-block
+	//
+	PEX_CALLBACK_ROUTINE_BLOCK CallbackBlock =
+		ExAllocateCallBack( PsCreateProcessNotifyCallback, FALSE );
+
+	if ( !CallbackBlock )
+	{
+		NPGCB_KDPRINT( "Failed to allocate callback block\n" );
+		return STATUS_INSUFFICIENT_RESOURCES;
+	}
+
+	NPGCB_KDPRINT( "Allocated Callback : %p\n", CallbackBlock );
+	NPGCB_KDPRINT( "                   : CallbackBlock->Function : %p\n", CallbackBlock->Function );
+	NPGCB_KDPRINT( "                   : CallbackBlock->Context  : %p\n", CallbackBlock->Context );
+
+	ULONG Counter = 0;
+
+	//
+	// this array (sizeof 64) contains all callback blocks
+	//
+	PVOID* Array = &*( PVOID* )
+		( ( UINT_PTR )NtosKrnlImageBase + RVA_PSP_CREATE_PROCESS_NOTIFY_ROUTINE );
+
+	while ( !ExCompareExchangeCallBack(
+		&Array[ Counter ],
+		CallbackBlock,
+		0 ) )
+	{
+		Counter++;
+
+		//
+		// only 64 callbacks available
+		// more than 64 entries mean there are no more available slots
+		//
+		if ( Counter >= PSP_CREATE_PROCESS_NOTIFY_MAX )
+		{
+			NPGCB_KDPRINT( "No Resource to register new callback!\n" );
+			return STATUS_INSUFFICIENT_RESOURCES;
+		}
+	}
+	
+	//
+	// increment count
+	// i am sorry for the terrible expression
+	//
+	InterlockedIncrement( (volatile LONG*)
+		&*( UINT32* )
+			( ( UINT_PTR )NtosKrnlImageBase + RVA_PSP_CREATE_PROCESS_NOTIFY_ROUTINE_COUNT ) );
+
+	NPGCB_KDPRINT( "Callback is now registered\n" );
+
+	return ntStatus;
+}
+
+NTSTATUS DispatchDriverEntry
+(
+    IN PDRIVER_OBJECT DriverObject,
+    IN PUNICODE_STRING RegistryPath
+)
+{
+	UNREFERENCED_PARAMETER( DriverObject );
+	UNREFERENCED_PARAMETER( RegistryPath );
+
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+
+	//
+	// prepare resources
+	//
+	ntStatus = NpgInitialize();
+
+	if ( !NT_SUCCESS( ntStatus ) )
+	{
+		NPGCB_KDPRINT( "Failed to initialize\n" );
+		return ntStatus;
+	}
+
+	//
+	// register PsCreateProcessNotifyCallback instead
+	// of using PsSetCreateProcessNotifyRoutine
+	//
+	// PatchGuard will never seeing you!
+	//
+	ntStatus = NpgRegisterCallback( PsCreateProcessNotifyCallback );
+
+	if ( !NT_SUCCESS( ntStatus ) )
+	{
+		NPGCB_KDPRINT( "Failed to register callback\n" );
+		return ntStatus;
+	}
+
+	return ntStatus;
+}
+
+//
+// this will be called when the driver being unloaded
+//
+VOID
+UnloadDriver
+(
+    IN PDRIVER_OBJECT DriverObject
+)
+{
+	UNREFERENCED_PARAMETER( DriverObject );
+}
+
+//
+// main entry point of this driver
+//
+NTSTATUS DriverEntry
+(
+    IN PDRIVER_OBJECT DriverObject,
+    IN PUNICODE_STRING RegistryPath
+)
+{
+    DriverObject->DriverUnload = UnloadDriver;
+
+	return DispatchDriverEntry( DriverObject, RegistryPath );
+}
---
+++ noPGcallback/main.h
@@ -0,0 +1,170 @@
+/*
+
+    MIT License
+
+    Copyright (c) 2021 Kento Oki
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+
+#pragma once
+
+#ifndef _NPGCB_MAIN_H_
+#define _NPGCB_MAIN_H_
+
+#include <ntifs.h>
+#include <ntddk.h>
+#include <windef.h>
+#include <ntimage.h>
+
+#include "dbg.h"
+
+#define RVA_EX_ALLOCATE_CALLBACK 0x781D10
+#define RVA_EX_COMPARE_EXCHANGE_CALLBACK 0x3AB4DC
+#define RVA_PSP_CREATE_PROCESS_NOTIFY_ROUTINE 0xCEC360
+#define RVA_PSP_CREATE_PROCESS_NOTIFY_ROUTINE_COUNT 0xD2D9C4
+
+#define PSP_CREATE_PROCESS_NOTIFY_MAX 64
+
+EXTERN_C int _fltused = 0x9875;
+
+DRIVER_INITIALIZE DriverInitialize;
+DRIVER_INITIALIZE DriverEntry;
+
+typedef struct _EX_CALLBACK_ROUTINE_BLOCK
+{
+    EX_RUNDOWN_REF RundownProtect;
+    PEX_CALLBACK_FUNCTION Function;
+    PVOID Context;
+} EX_CALLBACK_ROUTINE_BLOCK, * PEX_CALLBACK_ROUTINE_BLOCK;
+
+PEX_CALLBACK_ROUTINE_BLOCK( NTAPI* ExAllocateCallBack )( PVOID, UINT32 );
+BOOL( NTAPI* ExCompareExchangeCallBack )( PVOID, PVOID, ULONG );
+
+typedef enum _SYSTEM_INFORMATION_CLASS_EX
+{
+    SystemBasicInformation = 0,
+    SystemProcessorInformation = 1,
+    SystemPerformanceInformation = 2,
+    SystemTimeOfDayInformation = 3,
+    SystemPathInformation = 4,
+    SystemProcessInformation = 5,
+    SystemCallCountInformation = 6,
+    SystemDeviceInformation = 7,
+    SystemProcessorPerformanceInformation = 8,
+    SystemFlagsInformation = 9,
+    SystemCallTimeInformation = 10,
+    SystemModuleInformation = 11,
+    SystemLocksInformation = 12,
+    SystemStackTraceInformation = 13,
+    SystemPagedPoolInformation = 14,
+    SystemNonPagedPoolInformation = 15,
+    SystemHandleInformation = 16,
+    SystemObjectInformation = 17,
+    SystemPageFileInformation = 18,
+    SystemVdmInstemulInformation = 19,
+    SystemVdmBopInformation = 20,
+    SystemFileCacheInformation = 21,
+    SystemPoolTagInformation = 22,
+    SystemInterruptInformation = 23,
+    SystemDpcBehaviorInformation = 24,
+    SystemFullMemoryInformation = 25,
+    SystemLoadGdiDriverInformation = 26,
+    SystemUnloadGdiDriverInformation = 27,
+    SystemTimeAdjustmentInformation = 28,
+    SystemSummaryMemoryInformation = 29,
+    SystemMirrorMemoryInformation = 30,
+    SystemPerformanceTraceInformation = 31,
+    SystemObsolete0 = 32,
+    SystemExceptionInformation = 33,
+    SystemCrashDumpStateInformation = 34,
+    SystemKernelDebuggerInformation = 35,
+    SystemContextSwitchInformation = 36,
+    SystemRegistryQuotaInformation = 37,
+    SystemExtendServiceTableInformation = 38,
+    SystemPrioritySeperation = 39,
+    SystemVerifierAddDriverInformation = 40,
+    SystemVerifierRemoveDriverInformation = 41,
+    SystemProcessorIdleInformation = 42,
+    SystemLegacyDriverInformation = 43,
+    SystemCurrentTimeZoneInformation = 44,
+    SystemLookasideInformation = 45,
+    SystemTimeSlipNotification = 46,
+    SystemSessionCreate = 47,
+    SystemSessionDetach = 48,
+    SystemSessionInformation = 49,
+    SystemRangeStartInformation = 50,
+    SystemVerifierInformation = 51,
+    SystemVerifierThunkExtend = 52,
+    SystemSessionProcessInformation = 53,
+    SystemLoadGdiDriverInSystemSpace = 54,
+    SystemNumaProcessorMap = 55,
+    SystemPrefetcherInformation = 56,
+    SystemExtendedProcessInformation = 57,
+    SystemRecommendedSharedDataAlignment = 58,
+    SystemComPlusPackage = 59,
+    SystemNumaAvailableMemory = 60,
+    SystemProcessorPowerInformation = 61,
+    SystemEmulationBasicInformation = 62,
+    SystemEmulationProcessorInformation = 63,
+    SystemExtendedHandleInformation = 64,
+    SystemLostDelayedWriteInformation = 65,
+    SystemBigPoolInformation = 66,
+    SystemSessionPoolTagInformation = 67,
+    SystemSessionMappedViewInformation = 68,
+    SystemHotpatchInformation = 69,
+    SystemObjectSecurityMode = 70,
+    SystemWatchdogTimerHandler = 71,
+    SystemWatchdogTimerInformation = 72,
+    SystemLogicalProcessorInformation = 73,
+    SystemWow64SharedInformation = 74,
+    SystemRegisterFirmwareTableInformationHandler = 75,
+    SystemFirmwareTableInformation = 76,
+    SystemModuleInformationEx = 77,
+    SystemVerifierTriageInformation = 78,
+    SystemSuperfetchInformation = 79,
+    SystemMemoryListInformation = 80,
+    SystemFileCacheInformationEx = 81,
+    MaxSystemInfoClass = 82
+} SYSTEM_INFORMATION_CLASS_EX;
+
+typedef struct _RTL_PROCESS_MODULE_INFORMATION
+{
+    HANDLE Section;
+    PVOID MappedBase;
+    PVOID ImageBase;
+    ULONG ImageSize;
+    ULONG Flags;
+    USHORT LoadOrderIndex;
+    USHORT InitOrderIndex;
+    USHORT LoadCount;
+    USHORT OffsetToFileName;
+    UCHAR  FullPathName[ 256 ];
+} RTL_PROCESS_MODULE_INFORMATION, * PRTL_PROCESS_MODULE_INFORMATION;
+
+typedef struct _RTL_PROCESS_MODULES
+{
+    ULONG NumberOfModules;
+    RTL_PROCESS_MODULE_INFORMATION Modules[ 1 ];
+} RTL_PROCESS_MODULES, * PRTL_PROCESS_MODULES;
+
+EXTERN_C NTSYSCALLAPI NTSTATUS NTAPI ZwQuerySystemInformation( ULONG InfoClass, PVOID Buffer, ULONG Length, PULONG ReturnLength );
+
+#endif // _NPGCB_MAIN_H_
